182eff0a4b4111bd5394f61bb5a46aa5025462af
hankfrank <henryfrank1@yahoo.com>
2019-06-20 15:40:18 -0700

    Adding app.js to commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+const { Console } = require('console');
+const output = fs.createWriteStream('./stdout.log');
+const errorOutput = fs.createWriteStream('./stderr.log');
+// Custom simple logger
+const logger = new Console({ stdout: output, stderr: errorOutput });
+// use it like console
+const count = 5;
+logger.log('count: %d', count);
+
 
 var initialData = [];
 
